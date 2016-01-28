d96a4948e8d3e4063695fc8d760b4d5d4b13056c
parkjinhyun <qkwlsus123@naver.com>
2016-01-28 14:31:03 +0900

    시작화면

---
+++ app/src/main/java/com/cs/mju/hanmadang/Splash.java
@@ -0,0 +1,36 @@
+package com.cs.mju.hanmadang;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by park on 2016-01-28.
+ */
+public class Splash extends Activity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+			/*
+             *  원하는 화면을 띄워줌
+			 */
+
+            @Override
+            public void run() {
+                // 3초간 화면 보여주고 끝
+                // 화면 끝나고 메인 실행
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
